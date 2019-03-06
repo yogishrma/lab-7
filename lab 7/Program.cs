c569db95d80b96cc431b83fbeb11f6e037d9f0bb
visualstudio <visualstudio@122-PC40>
2019-03-06 11:27:58 -0500

    lab 7

--- lab 7/Program.cs
+++ lab 7/Program.cs
@@ -1,15 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace lab_7
+namespace DelegatesAndEvents
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            DelegateExercises delegateExercises = new DelegateExercises();
+            delegateExercises.Method3();
+            Console.ReadLine();
+        }
+    }
+
+    public class DelegateExercises
+    {
+        public delegate int MyDelegate(int intValue);
+
+        int Method1(int intMethod1)
+        {
+            return intMethod1 * 4;
+        }
+
+        int Method2(int intMethod1)
+        {
+            return intMethod1 * 20;
+        }
+
+        public void Method4(MyDelegate myDelegate)
+        {
+            for (int i = 1; i <= 5; i++)
+                System.Console.Write(myDelegate(i) + " ");
+        }
+
+        public void Method3()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            Method4(myDelegate);
+            myDelegate = new MyDelegate(Method2);
+            Method4(myDelegate);
         }
     }
 }
